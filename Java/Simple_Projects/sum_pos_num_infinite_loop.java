39676dab910b85bd3a5a4c360e5042f7462e6d2d
omar-fetian <omrf148@h-eng.helwan.edu.eg>
2020-09-22 11:08:44 +0200

    Create sum_pos_num_infinite_loop.java

---
+++ Java/Simple_Projects/sum_pos_num_infinite_loop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class sum_pos_num_infinite_loop {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num, sum = 0, counter = 0;
+        boolean flag_positive = true;
+
+        while (true){
+            System.out.print("Enter a positive number :");
+            num = in.nextInt();
+            if (num >= 0)   sum += num;
+            else {break;}
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
