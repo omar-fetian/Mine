187831f6580e29096d63cce224d07174219b38c9
omar-fetian <omrf148@h-eng.helwan.edu.eg>
2020-09-22 11:06:49 +0200

    Update and rename While_flag_1.java to sum_pos_num_flag.java

--- Java/Simple_Projects/sum_pos_num_flag.java
+++ Java/Simple_Projects/sum_pos_num_flag.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
-public class While_flag_1 {
+public class sum_pos_num_flag {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int num, sum = 0, counter = 0;
         boolean flag_positive = true;
-        System.out.println("Enter a positive number :");
+
         while (flag_positive){
+            System.out.print("Enter a positive number :");
             num = in.nextInt();
             if (num >= 0)   sum += num;
-            else {flag_positive = false; break;}
+            else {flag_positive = false;}
         }
         System.out.println("Sum = " + sum);
     }
