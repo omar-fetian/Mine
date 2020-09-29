a57b6c8bdaba546c3bbe5b52e93af63b33ce163a
omar-fetian <omrf148@h-eng.helwan.edu.eg>
2020-09-29 17:55:13 +0200

    Update find_employee_name.java

--- Java/Simple_Projects/find_employee_name.java
+++ Java/Simple_Projects/find_employee_name.java
@@ -9,8 +9,11 @@ public class Main {
         String[] employees_names = new String[employees_no];
         System.out.println("Enter the employees names and HR ID");
         for (int i = 0; i < employees_no; i++){
-            System.out.printf("Enter employee %d name:", i+1);      employees_names[i] = in.next();//problem here when entering names with 2 parts
-            System.out.printf("Enter %s  HR ID:", employees_names[i]);      employees_HR_ID[i] = in.nextInt();
+            System.out.printf("Enter employee no %d name:", i+1);
+            in.nextLine();
+            employees_names[i] = in.nextLine();
+            System.out.printf("Enter %s HR ID:",employees_names[i]);
+            employees_HR_ID[i] = in.nextInt();
         }
         System.out.print("Enter the employee HR ID to find his name:");     user_input = in.nextInt();
         System.out.println("the employee name is " + employees_names[arr_1D_search(employees_HR_ID, user_input)]);
@@ -22,5 +25,6 @@ public class Main {
         }
         return -1;
     }
+
 
 }
