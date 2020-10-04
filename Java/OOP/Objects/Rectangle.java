dd87c7152e713d2d62144b879e072680a8376da9
omar-fetian <omrf148@h-eng.helwan.edu.eg>
2020-10-04 15:21:07 +0200

    Update Rectangle.java

--- Java/OOP/Objects/Rectangle.java
+++ Java/OOP/Objects/Rectangle.java
@@ -9,16 +9,18 @@ public class Rectangle {
         length = 10;
         width = 20;
         System.out.println("a new room is created with length "+length +" and width "+width);
-    }
+    }//default length and width is set with the object creation
     public Rectangle(double l, double w){
         length = l;     width = w;
         System.out.println("a new room is created with length "+length +" and width "+width);
     }//construction executed with the object creation
     public void setLength(double L){
         if (L > 0)     length = L;
+        else System.out.println("data rejected, negative input");
     }
     public void setWidth(double W){
         if (W > 0)     width = W;
+        else System.out.println("data rejected, negative input");
     }
     public double getLength(){
         return length;
