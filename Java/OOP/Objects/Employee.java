821821273e1ce8909b026fe7a1ae93517110bcf9
omar-fetian <omrf148@h-eng.helwan.edu.eg>
2020-10-05 08:52:54 +0200

    Create Employee.java

---
+++ Java/OOP/Objects/Employee.java
@@ -0,0 +1,38 @@
+package com.MyClass;
+
+public class Employee {
+    private int Emp_ID;
+    private String Name, Depart;
+    private double Salary, Bonus;
+    private boolean Resident;
+
+    public Employee(){
+        Emp_ID = 0;     Name = null;      Depart =  null;     Salary = 0;     Bonus = 0;      Resident = false;
+    }
+    public Employee(int emp_ID, String name){
+        Emp_ID = emp_ID;        Name = name;
+    }
+    public Employee(int emp_ID, String name, boolean resident){
+        this(emp_ID, name);//instead of writing the whole code we can call the last constructor code using this
+        Resident = resident;
+    }
+    public Employee(int emp_ID, String name, String depart, double salary, double bonus, boolean resident){
+        this(emp_ID, name);    Depart = depart;    Salary = salary;    Bonus = bonus;  Resident = resident;
+    }
+    public void setSalary(double salary){
+        Salary = salary;
+    }
+    public void setSalary(double salary, double bonus){
+        Salary = salary;    Bonus = bonus;
+    }
+    public void setDepart(String depart){
+        Depart = depart;
+    }
+    public void setEmp_ID(int emp_ID){
+        Emp_ID = emp_ID;
+    }
+    public void printEmployeeData(){
+        System.out.printf("Employee ID: %d\nEmployee name : %s\nDepartment : %s\nSalary : %1.2f\nBonus : %1.2f\n",
+                Emp_ID, Name, Depart, Salary, Bonus);
+    }
+}
