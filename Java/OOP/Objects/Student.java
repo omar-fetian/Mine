ab80e349c7cae353543f1aaa0c3ac0f41f6d9da1
omar-fetian <omrf148@h-eng.helwan.edu.eg>
2020-10-08 04:38:38 +0200

    Create Student.java

---
+++ Java/OOP/Objects/Student.java
@@ -0,0 +1,46 @@
+package com.MyClass;
+public class Student extends Person {
+    private String Specialization;
+    private double GPA;
+    private int StudyLevel;
+    public Student() {
+    }
+    public Student(String name) {
+        super(name);
+    }
+    public Student(String name, int age, String nationality, String address, int studyLevel, String specialization, double GPA) {
+        super(name, age, nationality, address);
+        StudyLevel = studyLevel;
+        Specialization = specialization;
+        this.GPA = GPA;
+    }
+    public void setStudyLevel(int studyLevel) {
+        StudyLevel = studyLevel;
+    }
+    public void setSpecialization(String specialization) {
+        Specialization = specialization;
+    }
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+    public int getStudyLevel() {
+        return StudyLevel;
+    }
+    public String getSpecialization() {
+        return Specialization;
+    }
+    public double getGPA() {
+        return GPA;
+    }
+    public String toString() {
+        return "Student{" +
+                "Name='" + super.getName() + '\'' +
+                ", Address='" + super.getAddress() + '\'' +
+                ", Nationality='" + super.getNationality() + '\'' +
+                ", Age=" + super.getAge() +
+                ", StudyLevel='" + StudyLevel + '\'' +
+                ", Specialization='" + Specialization + '\'' +
+                ", GPA='" + GPA + '\'' +
+                '}';
+    }
+}
